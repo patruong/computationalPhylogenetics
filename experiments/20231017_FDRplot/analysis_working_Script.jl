7c28a43d4bdd1ae1a214cb0bc0ab95f6af165559
patruong <ptruong@kth.se>
2023-10-17 18:46:17 +0200

    update

--- experiments/20231017_FDRplot/analysis_working_Script.jl
+++ experiments/20231017_FDRplot/analysis_working_Script.jl
@@ -43,18 +43,36 @@ end
 
 function add_difference_cols_to_sim_settings(sim_settings, class1="simulator.omega.class0", class2="simulator.omega.class1")
     # sim_settings is pandas dfg
-    sim_settings[!, "difference.$class1"*".vs."*"$class2"] = sim_settings[!, class1] .- sim_settings[!, class2]
-    sim_settings[!, "bool.difference.$class1"*".vs."*"$class2"] = sim_settings[!, class1] .!= sim_settings[!, class2]
-    return sim_settings
+    diff_col = "difference.$class1" * ".vs." * "$class2"
+    bool_diff_col = "bool.difference.$class1" * ".vs." * "$class2"
+    sim_settings[!, diff_col] = sim_settings[!, class1] .- sim_settings[!, class2]
+    sim_settings[!, bool_diff_col] = sim_settings[!, class1] .!= sim_settings[!, class2]
+    return sim_settings, [diff_col, bool_diff_col]
 end
 
 function calculate_ω1_not_equal_ω2(df)
     #The probability that ω1 and ω2 are not equal is the complement of the probability that one is greater than the other:
     #P(ω1 ≠ ω2) = 1 - [P(ω1 > ω2) + P(ω2 > ω1)]
     #This is because there are only two possibilities: either ω1 is greater than ω2, ω2 is greater than ω1, or they are equal. So, you subtract the sum of the probabilities that ω1 is greater than ω2 and ω2 is greater than ω1 from 1 to find the probability that they are not equal.
-    df[!, "P(ω1 ≠ ω2)"] = 1 .- (difFUBAR_res[!, "P(ω1 > ω2)"] .+ difFUBAR_res[!, "P(ω2 > ω1)"])
+    df[!, "P(ω1 ≠ ω2)"] = (difFUBAR_res[!, "P(ω1 > ω2)"] .+ difFUBAR_res[!, "P(ω2 > ω1)"])
     return df
 end
+
+function add_codon_site(df)
+    df[!, :Codon_site] = axes(df, 1)
+    return df
+end
+
+function cumsum_actual_positive(df)
+    df[!, :cumsum_actual_positive] = cumsum(df[!, :actual_difference])
+    return df
+end
+
+function cumsum_actual_negative(df) # Actual negatives
+    df[!, :cumsum_actual_negative] = cumsum(df[!, :actual_difference] .== false)
+    return df
+end
+
 
 
 # sim.0.replicate.1 
@@ -64,31 +82,129 @@ difFUBAR_output_dir = "experiments/20231011_run_both_pipelines_on_sample/run_dif
 difFUBAR_res = CSV.File(difFUBAR_output_dir * "results_posteriors.csv", delim=",", header=true) |> DataFrame
 difFUBAR_res = calculate_ω1_not_equal_ω2(difFUBAR_res)
 
-
-sort(difFUBAR_res)
-
-sorted_df = sort(difFUBAR_res, order=(:"P(ω1 ≠ ω2)",))
-
-typeof(difFUBAR_res)
-sort(difFUBAR_res, "P(ω1 ≠ ω2)")
-
 # contrastFEL use Q-value 
 contrastFEL_output_dir = "contrastFEL_data/omnibus/"
 contrastFEL_res = read_contrastFEL_output_JSON(contrastFEL_output_dir * "sims.0.settings.replicate.1.FEL.json")
-
+contrastFEL_res = add_codon_site(contrastFEL_res)
 
 # Simulator settings
 simulator_settings_dir = "contrastFEL_data/omnibus/"
 simulator_settings = CSV.File(simulator_settings_dir * "sims.0.settings.tsv", delim="\t", header=true) |> DataFrame
-add_difference_cols_to_sim_settings(simulator_settings, "simulator.omega.class0", "simulator.omega.class1")
+simulator_settings, settings_cols = add_difference_cols_to_sim_settings(simulator_settings, "simulator.omega.class0", "simulator.omega.class1")
+sum(simulator_settings[!, settings_cols[2]])
+
+difFUBAR_res[!, "actual_difference"] = simulator_settings[!, settings_cols[2]]
+contrastFEL_res[!, "actual_difference"] = simulator_settings[!, settings_cols[2]]
+difFUBAR_res = sort(difFUBAR_res, "P(ω1 ≠ ω2)", rev=true)
+contrastFEL_res = sort!(contrastFEL_res, "Q-value (overall)")
 
 
 
-difFUBAR_res
-contrastFEL_res
-sim_settings
 
-difFUBAR_res
+###################################################################
+###################################################################
+###################################################################
+###################################################################
+###################################################################
+###################################################################
+###################################################################
+
+
+df = difFUBAR_res
+# Assuming you have a DataFrame 'df' with the P(ω1 ≠ ω2) column
+# and a boolean list 'actual_difference'
+
+# Calculate TPR and FPR
+threshold = 0.5  # Set your desired threshold
+
+function calculate_TRP_and_FPR(df, threshold)
+    predictions = df[!, "P(ω1 ≠ ω2)"] .> threshold
+    actual_difference = df[!, "actual_difference"]
+    TP = sum(predictions .& actual_difference)
+    TN = sum((predictions .== false) .& (actual_difference .== false))
+    FP = sum(predictions .& (actual_difference .== false))
+    FN = sum((predictions .== false) .& actual_difference)
+    TPR = TP / (TP + FN)  # True Positive Rate (Sensitivity or Recall)
+    FPR = FP / (FP + TN)  # False Positive Rate
+    return TP, TN, FP, FN, TPR, FPR
+end
+
+
+using DataFrames
+
+# Define your function to accept a DataFrame, a column name for probabilities, and a threshold
+function calculate_TRP_and_FPR(df, prob_column, threshold)
+    predictions = df[!, prob_column] .> threshold
+    actual_difference = df[!, "actual_difference"]
+    TP = sum(predictions .& actual_difference)
+    TN = sum((predictions .== false) .& (actual_difference .== false))
+    FP = sum(predictions .& (actual_difference .== false))
+    FN = sum((predictions .== false) .& actual_difference)
+    TPR = TP / (TP + FN)  # True Positive Rate (Sensitivity or Recall)
+    FPR = FP / (FP + TN)  # False Positive Rate
+    return TP, TN, FP, FN, TPR, FPR
+end
+
+# Assuming you have a DataFrame 'df', a column 'prob_column', and a threshold
+df = DataFrame(
+    prob_column=[0.6, 0.7, 0.4, 0.8],
+    actual_difference=[true, false, true, false]
+)
+
+threshold_value = 0.5  # Set your desired threshold
+
+# Apply the function and store the results in variables
+TP, TN, FP, FN, TPR, FPR = calculate_TRP_and_FPR(df, :prob_column, threshold_value)
+
+# Display the results
+println("True Positives (TP): $TP")
+println("True Negatives (TN): $TN")
+println("False Positives (FP): $FP")
+println("False Negatives (FN): $FN")
+println("True Positive Rate (TPR): $TPR")
+println("False Positive Rate (FPR): $FPR")
+
+# Output TPR and FPR
+#println("True Positive Rate (TPR): $TPR")
+#println("False Positive Rate (FPR): $FPR")
 
 
 
+using DataFrames
+df = DataFrame(a=1:7, b=4:10, c=7:13)
+df[!, "test test"] = df[!, :a]
+df[!, "test"] = df[!, :a]
+
+function calculate_result(x, y, df)
+    a_sum = sum(df[1:x, :a])
+    return a_sum * y
+end
+
+function test2(x, y)
+    return 2x + y
+end
+transform(df, AsTable([:a, :c]) => ByRow(x -> (calculate_result(x.a, x.c, df), 4x.c)) => ["ae what", "f"])
+
+
+df = difFUBAR_res
+df[!, "threshold"] = df[!, "P(ω1 ≠ ω2)"]
+
+function calculate_TRP_and_FPR(df, threshold)
+    predictions = df[!, "P(ω1 ≠ ω2)"] .> threshold
+    actual_difference = df[!, "actual_difference"]
+    TP = sum(predictions .& actual_difference)
+    TN = sum((predictions .== false) .& (actual_difference .== false))
+    FP = sum(predictions .& (actual_difference .== false))
+    FN = sum((predictions .== false) .& actual_difference)
+    TPR = TP / (TP + FN)  # True Positive Rate (Sensitivity or Recall)
+    FPR = FP / (FP + TN)  # False Positive Rate
+    return TP, TN, FP, FN, TPR, FPR
+end
+
+res = transform(df, AsTable([:threshold]) => ByRow(x -> (calculate_TRP_and_FPR(df, x.threshold))) => [:TP, :TN, :FP, :FN, :TPR, :FPR])
+res
+using Plots
+plot(res.FPR, res.TPR, xlabel="FPR", ylabel="TPR", label="Data") # This should be roughly equivalent to FPR?
+
+
+#Nice work on this
