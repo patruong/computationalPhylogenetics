395dc2346a1d853ea399c70dbde8676b1c6e171d
patruong <ptruong@kth.se>
2023-10-17 23:49:33 +0200

    update FDR plot

--- experiments/20231017_FDRplot/analysis_working_Script.jl
+++ experiments/20231017_FDRplot/analysis_working_Script.jl
@@ -4,6 +4,7 @@ using Pkg
 using CSV
 using JSON
 using DataFrames
+using Plots
 
 function read_contrastFEL_output_JSON(file_name)
     # Open the JSON file and read each line as a JSON object
@@ -73,23 +74,57 @@ function cumsum_actual_negative(df) # Actual negatives
     return df
 end
 
+function calculate_TRP_and_FPR_difFUBAR(df)
+    df[!, "threshold"] = df[!, "P(ω1 ≠ ω2)"]
 
+    function calculate_TRP_and_FPR_difFUBAR_helper(df, threshold)
+        predictions = df[!, "P(ω1 ≠ ω2)"] .> threshold
+        actual_difference = df[!, "actual_difference"]
+        TP = sum(predictions .& actual_difference)
+        TN = sum((predictions .== false) .& (actual_difference .== false))
+        FP = sum(predictions .& (actual_difference .== false))
+        FN = sum((predictions .== false) .& actual_difference)
+        TPR = TP / (TP + FN)  # True Positive Rate (Sensitivity or Recall)
+        FPR = FP / (FP + TN)  # False Positive Rate
+        return TP, TN, FP, FN, TPR, FPR
+    end
+
+    res = transform(df, AsTable([:threshold]) => ByRow(x -> (calculate_TRP_and_FPR_difFUBAR_helper(df, x.threshold))) => [:TP, :TN, :FP, :FN, :TPR, :FPR])
+    return res
+end
+
+function calculate_TPR_and_FPR_contrastFEL(df)
+    df[!, "threshold"] = df[!, "Q-value (overall)"]
+    function calculate_TRP_and_FPR_contrastFEL_helper(df, threshold)
+        predictions = df[!, "Q-value (overall)"] .< threshold
+        actual_difference = df[!, "actual_difference"]
+        TP = sum(predictions .& actual_difference)
+        TN = sum((predictions .== false) .& (actual_difference .== false))
+        FP = sum(predictions .& (actual_difference .== false))
+        FN = sum((predictions .== false) .& actual_difference)
+        TPR = TP / (TP + FN)  # True Positive Rate (Sensitivity or Recall)
+        FPR = FP / (FP + TN)  # False Positive Rate
+        return TP, TN, FP, FN, TPR, FPR
+    end
+    res = transform(df, AsTable([:threshold]) => ByRow(x -> (calculate_TRP_and_FPR_contrastFEL_helper(df, x.threshold))) => [:TP, :TN, :FP, :FN, :TPR, :FPR])
+    return res
+end
 
 # sim.0.replicate.1 
 
 #difFUBAR_Res
-difFUBAR_output_dir = "experiments/20231011_run_both_pipelines_on_sample/run_difFUBAR_omnibus/output/omnibus/pos_thresh_0.75/sim.0.replicate.1/REFERENCEvTEST/"
+difFUBAR_output_dir = "experiments/20231011_run_both_pipelines_on_sample/run_difFUBAR_omnibus/output/omnibus/pos_thresh_0.95/sim.421.replicate.1/REFERENCEvTEST/"
 difFUBAR_res = CSV.File(difFUBAR_output_dir * "results_posteriors.csv", delim=",", header=true) |> DataFrame
 difFUBAR_res = calculate_ω1_not_equal_ω2(difFUBAR_res)
 
 # contrastFEL use Q-value 
 contrastFEL_output_dir = "contrastFEL_data/omnibus/"
-contrastFEL_res = read_contrastFEL_output_JSON(contrastFEL_output_dir * "sims.0.settings.replicate.1.FEL.json")
+contrastFEL_res = read_contrastFEL_output_JSON(contrastFEL_output_dir * "sims.421.settings.replicate.1.FEL.json")
 contrastFEL_res = add_codon_site(contrastFEL_res)
 
 # Simulator settings
 simulator_settings_dir = "contrastFEL_data/omnibus/"
-simulator_settings = CSV.File(simulator_settings_dir * "sims.0.settings.tsv", delim="\t", header=true) |> DataFrame
+simulator_settings = CSV.File(simulator_settings_dir * "sims.421.settings.tsv", delim="\t", header=true) |> DataFrame
 simulator_settings, settings_cols = add_difference_cols_to_sim_settings(simulator_settings, "simulator.omega.class0", "simulator.omega.class1")
 sum(simulator_settings[!, settings_cols[2]])
 
@@ -98,113 +133,11 @@ contrastFEL_res[!, "actual_difference"] = simulator_settings[!, settings_cols[2]
 difFUBAR_res = sort(difFUBAR_res, "P(ω1 ≠ ω2)", rev=true)
 contrastFEL_res = sort!(contrastFEL_res, "Q-value (overall)")
 
+difFUBAR_res = calculate_TRP_and_FPR_difFUBAR(difFUBAR_res)
+contrastFEL_res = calculate_TPR_and_FPR_contrastFEL(contrastFEL_res)
+
+plot(res.FPR, res.TPR, xlabel="FPR", ylabel="TPR", label="difFUBAR") # This should be roughly equivalent to FPR?
+plot(res.FPR, res.TPR, xlabel="FPR", ylabel="TPR", label="contrastFEL") # This should be roughly equivalent to FPR?
 
 
 
-###################################################################
-###################################################################
-###################################################################
-###################################################################
-###################################################################
-###################################################################
-###################################################################
-
-
-df = difFUBAR_res
-# Assuming you have a DataFrame 'df' with the P(ω1 ≠ ω2) column
-# and a boolean list 'actual_difference'
-
-# Calculate TPR and FPR
-threshold = 0.5  # Set your desired threshold
-
-function calculate_TRP_and_FPR(df, threshold)
-    predictions = df[!, "P(ω1 ≠ ω2)"] .> threshold
-    actual_difference = df[!, "actual_difference"]
-    TP = sum(predictions .& actual_difference)
-    TN = sum((predictions .== false) .& (actual_difference .== false))
-    FP = sum(predictions .& (actual_difference .== false))
-    FN = sum((predictions .== false) .& actual_difference)
-    TPR = TP / (TP + FN)  # True Positive Rate (Sensitivity or Recall)
-    FPR = FP / (FP + TN)  # False Positive Rate
-    return TP, TN, FP, FN, TPR, FPR
-end
-
-
-using DataFrames
-
-# Define your function to accept a DataFrame, a column name for probabilities, and a threshold
-function calculate_TRP_and_FPR(df, prob_column, threshold)
-    predictions = df[!, prob_column] .> threshold
-    actual_difference = df[!, "actual_difference"]
-    TP = sum(predictions .& actual_difference)
-    TN = sum((predictions .== false) .& (actual_difference .== false))
-    FP = sum(predictions .& (actual_difference .== false))
-    FN = sum((predictions .== false) .& actual_difference)
-    TPR = TP / (TP + FN)  # True Positive Rate (Sensitivity or Recall)
-    FPR = FP / (FP + TN)  # False Positive Rate
-    return TP, TN, FP, FN, TPR, FPR
-end
-
-# Assuming you have a DataFrame 'df', a column 'prob_column', and a threshold
-df = DataFrame(
-    prob_column=[0.6, 0.7, 0.4, 0.8],
-    actual_difference=[true, false, true, false]
-)
-
-threshold_value = 0.5  # Set your desired threshold
-
-# Apply the function and store the results in variables
-TP, TN, FP, FN, TPR, FPR = calculate_TRP_and_FPR(df, :prob_column, threshold_value)
-
-# Display the results
-println("True Positives (TP): $TP")
-println("True Negatives (TN): $TN")
-println("False Positives (FP): $FP")
-println("False Negatives (FN): $FN")
-println("True Positive Rate (TPR): $TPR")
-println("False Positive Rate (FPR): $FPR")
-
-# Output TPR and FPR
-#println("True Positive Rate (TPR): $TPR")
-#println("False Positive Rate (FPR): $FPR")
-
-
-
-using DataFrames
-df = DataFrame(a=1:7, b=4:10, c=7:13)
-df[!, "test test"] = df[!, :a]
-df[!, "test"] = df[!, :a]
-
-function calculate_result(x, y, df)
-    a_sum = sum(df[1:x, :a])
-    return a_sum * y
-end
-
-function test2(x, y)
-    return 2x + y
-end
-transform(df, AsTable([:a, :c]) => ByRow(x -> (calculate_result(x.a, x.c, df), 4x.c)) => ["ae what", "f"])
-
-
-df = difFUBAR_res
-df[!, "threshold"] = df[!, "P(ω1 ≠ ω2)"]
-
-function calculate_TRP_and_FPR(df, threshold)
-    predictions = df[!, "P(ω1 ≠ ω2)"] .> threshold
-    actual_difference = df[!, "actual_difference"]
-    TP = sum(predictions .& actual_difference)
-    TN = sum((predictions .== false) .& (actual_difference .== false))
-    FP = sum(predictions .& (actual_difference .== false))
-    FN = sum((predictions .== false) .& actual_difference)
-    TPR = TP / (TP + FN)  # True Positive Rate (Sensitivity or Recall)
-    FPR = FP / (FP + TN)  # False Positive Rate
-    return TP, TN, FP, FN, TPR, FPR
-end
-
-res = transform(df, AsTable([:threshold]) => ByRow(x -> (calculate_TRP_and_FPR(df, x.threshold))) => [:TP, :TN, :FP, :FN, :TPR, :FPR])
-res
-using Plots
-plot(res.FPR, res.TPR, xlabel="FPR", ylabel="TPR", label="Data") # This should be roughly equivalent to FPR?
-
-
-#Nice work on this
