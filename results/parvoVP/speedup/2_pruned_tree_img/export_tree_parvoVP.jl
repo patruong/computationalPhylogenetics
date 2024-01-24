2b8a131a5795ae123c98adc5b03b9240e9c0b2bf
patruong <ptruong@kth.se>
2024-01-24 12:46:48 +0000

    update

--- results/parvoVP/speedup/2_pruned_tree_img/export_tree_parvoVP.jl
+++ results/parvoVP/speedup/2_pruned_tree_img/export_tree_parvoVP.jl
@@ -63,16 +63,64 @@ tags, tag_colors, remove_tags = select_analysis_tags_from_newick_tree(original_t
 
 tree, tags, tag_colors, analysis_name = CodonMolecularEvolution.difFUBAR_init(analysis_name, treestring, tags, tag_colors, exports=exports, verbosity=verbosity)
 tree, alpha, beta, GTRmat, F3x4_freqs, eq_freqs = CodonMolecularEvolution.difFUBAR_global_fit(seqnames, seqs, tree, CodonMolecularEvolution.generate_tag_stripper(tags), MolecularEvolution.universal_code, verbosity=verbosity)
+
+################################
+# printout pre-computed clades #
+################################
+
+function save_tree(tree, output_name=analysis_name * "_pruned_tagged_input_tree.svg")
+    strip_tags_from_name = CodonMolecularEvolution.generate_tag_stripper(tags)
+    color_dict = Dict(zip(getnodelist(tree), [tag_colors[model_ind(n.name, tags)] for n in getnodelist(tree)]))
+    label_dict = Dict(zip(getnodelist(tree), [strip_tags_from_name(n.name) for n in getnodelist(tree)]))
+    img = tree_draw(tree, canvas_width=30cm, canvas_height=(3 + length(getleaflist(tree)) / 5)cm,
+        draw_labels=true, dot_color_dict=color_dict,
+        line_color_dict=color_dict, line_width=0.3, min_dot_size=0.01,
+        nodelabel_dict=label_dict)
+
+    #img |> SVG(analysis_name * "_tagged_input_tree.svg", 15cm, (3 + length(getleaflist(tree)) / 5)cm)
+    img |> SVG(output_name, 30cm, (3 + length(getleaflist(tree)) / 5)cm)
+end
+
+function check_purity_from_node_and_forward_in_time(tree)
+    # collect all nodes under and see if they are the same group
+    # if return true that means everything under the tree is pure (the tree node can be differnt though)
+    node_groups = []
+    for node in getnodelist(tree)
+        if tree.name != node.name # we only check nodes under the input tree or subtree
+            push!(node_groups, model_ind(node.name, tags))
+        end
+    end
+    if length(unique(node_groups)) == 1
+        return true
+    else
+        return false
+    end
+end
+
+pure_clades = []
+function traverse_tree_to_check_for_pure_clades(pure_clades, tree)
+    for child in tree.children
+        if check_purity_from_node_and_forward_in_time(child)
+            #print(child.name)
+            #println(" PURE")
+            # add this node, as everything under this node is pure
+            push!(pure_clades, child)
+        else
+            #print(child.name)
+            #println(" not pure") # keep recursion to se if we go forward in time if we can find pure clades
+            traverse_tree_to_check_for_pure_clades(pure_clades, child)
+        end
+    end
+    return pure_clades
+end
+
+pure_clades = traverse_tree_to_check_for_pure_clades(pure_clades, tree)
+
+subtree_1 = pure_clades[1]
+subtree_2 = pure_clades[2]
+save_tree(subtree_1, "subtree1.svg")
+save_tree(subtree_2, "subtree2.svg")
+
 con_lik_matrix, _, codon_param_vec, alphagrid, omegagrid, _ = CodonMolecularEvolution.difFUBAR_grid_pruned_3(tree, tags, GTRmat, F3x4_freqs, MolecularEvolution.universal_code,
     verbosity=verbosity, foreground_grid=6, background_grid=4)
-
-strip_tags_from_name = CodonMolecularEvolution.generate_tag_stripper(tags)
-color_dict = Dict(zip(getnodelist(tree), [tag_colors[model_ind(n.name, tags)] for n in getnodelist(tree)]))
-label_dict = Dict(zip(getnodelist(tree), [strip_tags_from_name(n.name) for n in getnodelist(tree)]))
-img = tree_draw(tree, canvas_width=30cm, canvas_height=(3 + length(getleaflist(tree)) / 5)cm,
-    draw_labels=true, dot_color_dict=color_dict,
-    line_color_dict=color_dict, line_width=0.3, min_dot_size=0.01,
-    nodelabel_dict=label_dict)
-
-#img |> SVG(analysis_name * "_tagged_input_tree.svg", 15cm, (3 + length(getleaflist(tree)) / 5)cm)
-img |> SVG(analysis_name * "_pruned_tagged_input_tree.svg", 30cm, (3 + length(getleaflist(tree)) / 5)cm)
+save_tree(subtree_2, "pruned_tree.svg")
