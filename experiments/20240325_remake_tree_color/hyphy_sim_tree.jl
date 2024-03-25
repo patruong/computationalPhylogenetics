c4821eca4969acca02c0b68c274a3bf36251c82b
patruong <ptruong@kth.se>
2024-03-25 10:47:00 +0000

    update

---
+++ experiments/20240325_remake_tree_color/hyphy_sim_tree.jl
@@ -0,0 +1,65 @@
+using Revise # for code dev
+# dev ~/git/CodonMolecularEvolution.jl/ # in package environment
+# dev ~/git/MolecularEvolution.jl/ # in package environment
+using MolecularEvolution #read_fasta
+using CodonMolecularEvolution
+using Compose
+using ArgParse
+using CSV
+using DataFrames
+using Tables
+
+
+#cd("/home/patrick/git/computationalPhylogenetics/")
+cd("/home/patrick/git/computationalPhylogenetics/contrastFEL_data/omnibus/")
+
+
+analysis_name = "sims.279"
+exports = true
+verbosity = 1
+iters = 2500
+pos_thresh = 0.95
+output_csv = "output"
+
+
+fasta_file = "$analysis_name.settings.replicate.1"
+newick_file = "$analysis_name.nwk"
+
+seqnames, seqs = import_hyphy_simulated_FASTA(fasta_file) #for sim_tree_fixed
+
+treestring_group_labeled, treestring, group_tags, original_tags, tag_colors = import_grouped_label_tree(newick_file)
+
+comparisons_idx = [i for i in 1:length(original_tags)]
+pairwise_comparisons = [[i, j] for i in comparisons_idx for j in comparisons_idx if i < j]
+
+# Printout for information
+group_name = ["$i = $(original_tags[i])" for i in 1:length(original_tags)]
+pairwise_comparisons_names = [[original_tags[i], original_tags[j]] for i in comparisons_idx for j in comparisons_idx if i < j]
+
+# group_name printout
+group_meta_data_file_name = output_csv * "group_meta_data.txt"
+group_meta_data_file = open(group_meta_data_file_name, "w")
+for element in group_name
+    println(group_meta_data_file, "Groups")
+    println(group_meta_data_file, element)
+end
+println(group_meta_data_file, "")
+
+# pairwise comparison printout
+for element in pairwise_comparisons_names
+    println(group_meta_data_file, "Comparisons")
+    println(group_meta_data_file, element)
+end
+close(group_meta_data_file)
+
+tag_pos = [1, 2]
+tag1 = chop(original_tags[tag_pos[1]], head=1, tail=1)
+tag2 = chop(original_tags[tag_pos[2]], head=1, tail=1)
+tags, tag_colors, remove_tags = select_analysis_tags_from_newick_tree(original_tags, tag_colors, tag_pos)
+
+cd("/home/patrick/git/computationalPhylogenetics/experiments/20240325_remake_tree_color")
+tree, tags, tag_colors, analysis_name = CodonMolecularEvolution.difFUBAR_init(analysis_name, treestring, tags, tag_colors, exports=exports, verbosity=verbosity)
+
+
+#tree, alpha, beta, GTRmat, F3x4_freqs, eq_freqs = CodonMolecularEvolution.difFUBAR_global_fit(seqnames, seqs, tree, CodonMolecularEvolution.generate_tag_stripper(tags), MolecularEvolution.universal_code, verbosity=verbosity)
+#GTRmat # what is going on with this?
