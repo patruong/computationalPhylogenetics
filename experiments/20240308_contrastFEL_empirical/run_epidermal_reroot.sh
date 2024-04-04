a8092dec7350dae68669695786e4c3463c054998
patruong <ptruong@kth.se>
2024-04-04 05:46:45 +0000

    update res

--- experiments/20240308_contrastFEL_empirical/run_epidermal_reroot.sh
+++ experiments/20240308_contrastFEL_empirical/run_epidermal_reroot.sh
@@ -6,5 +6,5 @@ input_tree=data/epidermal_reroot_retagged.nwk
 mkdir -p "$output_dir/treesurgery_and_parallel"
 mkdir -p "$output_dir/contrastfel"
 
-(time julia run_rubisco.jl) 2>&1 | tee "$output_dir/treesurgery_and_parallel/difFUBAR_output.txt" "$output_dir/treesurgery_and_parallel/time_output.txt"
-(time hyphy contrast-fel --alignment $input_fasta  --tree $input_tree --branch-set C3 --branch-set C4 --p-value 1.00 --q-value 1.00 --cpu 20 --output $output_dir/contrastfel/contrastfel.FEL.json) 2>&1 | tee "$output_dir/contrastfel/constrastfel_output.txt" "$output_dir/contrastfel/time_output.txt"
+(time julia run_epidermal_reroot.jl) 2>&1 | tee "$output_dir/treesurgery_and_parallel/difFUBAR_output.txt" "$output_dir/treesurgery_and_parallel/time_output.txt"
+#(time hyphy contrast-fel --alignment $input_fasta  --tree $input_tree --branch-set Physaria --branch-set background --p-value 1.00 --q-value 1.00 --cpu 20 --output $output_dir/contrastfel/contrastfel.FEL.json) 2>&1 | tee "$output_dir/contrastfel/constrastfel_output.txt" "$output_dir/contrastfel/time_output.txt"
